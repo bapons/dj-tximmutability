d6c8a779256a6f11489f8975c88c8d71ef61bf37
Tomeu Pons Perello <1244511+bapons@users.noreply.github.com>
2021-10-21 17:48:30 +0200

    merge (#4)
    * rename ImmutabilityRule attr mutable_states to mutable_values
    
    * remove ImmutabilityRule attr 'mutable_fields'
    
    * reformat class names and messages
    
    * Refactor Rule & models
    
    Rule: Rename class , rename attrib, Model: refactor BaseImmutableModelUpdate
    
    * adapt tests classname renamed 'MutabilityRule'
    
    * raise ValidationError from django.core
    
    * rename attr field_name to field_rule
    
    * Comments rule
    
    * edit setup black
    
    * Implement FieldTracker, change nomenclature 'Immutability' to 'mutability', add django translation
    
    * add translation EN SP
    
    * update setup p3, requirements ...
    
    * Rule, remove __name__ mehtod
    
    * Implement FieldTracker, change nomenclature 'Immutability' to 'mutability', add django translation
    
    * add authors
    
    * Refactor tests
    
    * black to 80 line length
    
    * black format
    
    * minimum changes, format & edit logs
    
    * update gitignore
    
    * update gitignore
    
    * translations - add mo & updated po
    
    * update setup.py & tox remove python versions under 3.5
    
    * update setup
    
    Co-authored-by: Tomeu Pons <tomeu.pons@txerpa.com>

--- setup.py
+++ setup.py
@@ -9,13 +9,16 @@ try:
 except ImportError:
     from distutils.core import setup
 
+CURRENT_PYTHON = sys.version_info[:2]
+
 
 def get_version(*file_paths):
     """Retrieves the version from tximmutability/__init__.py"""
     filename = os.path.join(os.path.dirname(__file__), *file_paths)
     version_file = open(filename).read()
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
+    version_match = re.search(
+        r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M
+    )
     if version_match:
         return version_match.group(1)
     raise RuntimeError('Unable to find version string.')
@@ -23,10 +26,10 @@ def get_version(*file_paths):
 
 version = get_version("tximmutability", "__init__.py")
 
-
 if sys.argv[-1] == 'publish':
     try:
         import wheel
+
         print("Wheel version: ", wheel.__version__)
     except ImportError:
         print('Wheel library missing. Please run "pip install wheel"')
@@ -41,13 +44,17 @@ if sys.argv[-1] == 'tag':
     os.system("git push --tags")
     sys.exit()
 
-readme = open('README.rst').read()
-history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+with open('README.rst', 'r') as f:
+    readme = f.read()
+# readme = open('README.rst').read()
+with open('HISTORY.rst', 'r') as f:
+    history = f.read().replace('.. :changelog:', '')
+# history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='dj-tximmutability',
     version=version,
-    description="""Your project description goes here""",
+    description='Mutability rules for Django models.',
     long_description=readme + '\n\n' + history,
     author='Marija Milicevic',
     author_email='marija.milicevic@txerpa.com',
@@ -56,24 +63,33 @@ setup(
         'tximmutability',
     ],
     include_package_data=True,
-    install_requires=[],
-    license="MIT",
+    install_requires=["django>=2.2,<=3", "django-model-utils==4.1.1"],
+    python_requires=">=3.5",
+    license="BSD",
     zip_safe=False,
-    keywords='dj-tximmutability',
+    keywords=['django', 'tximmutability', 'immutability', 'mutability'],
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
         'Framework :: Django',
         'Framework :: Django :: 1.8',
         'Framework :: Django :: 1.9',
         'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
+        'Framework :: Django :: 3.2',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
+        'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Utilities',
     ],
 )
